e92d41661360acafc54ae9ccbb83a1bce82b4b14
prashanth0107 <143941344+prashanth0107@users.noreply.github.com>
2023-09-03 17:42:04 +0530

    Add files via upload

---
+++ PythonCourse.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[3]:
+
+
+'''a=10
+b=15
+print("sub of a and b",a-b)'''
+
+
+# In[4]:
+
+
+M=15
+m=20
+datatype(m)
+
+
+# In[5]:
+
+
+'''a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+print("sum of a and b:",(((a+b)*b)-a))'''
+
+
+# In[6]:
+
+
+print(a+b)
+
+
+# In[7]:
+
+
+type(a+b)
+
+
+# In[8]:
+
+
+print(a+b)
+
+
+# In[11]:
+
+
+x=str
+X=133
+print(type(X))
+print(type(x))
+
+
+# In[5]:
+
+
+name= "Prashanth"
+print("hello",name)
+
+
+# # Data type
+
+# # 1.STR
+
+# In[1]:
+
+
+x="prashanth"
+
+
+# In[2]:
+
+
+type(x)
+
+
+# In[8]:
+
+
+str="srinivasa rao..... \n Kumari"
+
+
+# In[9]:
+
+
+print(str)
+
+
+# In[10]:
+
+
+A="123"
+
+
+# In[11]:
+
+
+type(A)
+
+
+# # Slicing
+
+# In[18]:
+
+
+print(x[3:-1])
+
+
+# In[19]:
+
+
+print(x)
+
+
+# In[25]:
+
+
+x1="Hello World!|"
+
+
+# In[26]:
+
+
+x.lower()
+
+
+# In[27]:
+
+
+x.upper()
+
+
+# In[28]:
+
+
+x.strip()
+
+
+# # Concatination
+
+# In[37]:
+
+
+Y=123
+Y1=456
+total=Y+Y1
+print("full name is:",fullname)
+
+
+# In[34]:
+
+
+Y.count("a")
+
+
+# In[40]:
+
+
+q="Prashanth"
+
+
+# In[42]:
+
+
+q.count("a")
+
+
+# In[46]:
+
+
+q.startswith("s")
+
+
+# In[47]:
+
+
+q.find("a")
+
+
+# In[54]:
+
+
+q1="12".join(q)
+
+
+# In[51]:
+
+
+print(q1)
+
+
+# In[52]:
+
+
+q1
+
+
+# # Boolean Functionlity
+
+# In[55]:
+
+
+print(100>99)
+
+
+# In[56]:
+
+
+print(10==100)
+
+
+# In[83]:
+
+
+a=20
+b=20
+if a<b:
+    print("a is less than b")
+else:
+        print("b is less than a")
+
+
+# In[84]:
+
+
+list=[1,2,3,"prashanth",55,"ardashhfsf",["none"]]
+
+
+# In[85]:
+
+
+list
+
+
+# In[86]:
+
+
+list[0]
+
+
+# In[87]:
+
+
+list[-3:]
+
+
+# In[88]:
+
+
+list.append("skoda")
+
+
+# In[89]:
+
+
+list
+
+
+# In[92]:
+
+
+list.insert(1,"skoda")
+
+
+# In[91]:
+
+
+list
+
+
+# In[96]:
+
+
+list.remove("skoda")
+
+
+# In[102]:
+
+
+list
+
+
+# In[105]:
+
+
+list.remove(2)
+
+
+# In[106]:
+
+
+list
+
+
+# In[ ]:
+
+
+
+
